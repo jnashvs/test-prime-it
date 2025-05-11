4ff7296ffb222efbaef3f29a11e1b7a95408c072
jnashvs <31808210+jnashvs@users.noreply.github.com>
2025-05-11 15:00:42 +0100

    update the build.sh

--- src/build.sh
+++ src/build.sh
@@ -3,7 +3,7 @@
 echo "Running deploy script"
 
 echo "[1/5] Pulling from GitHub"
-git pull origin
+git pull origin main
 
 echo "[2/5] Creating database if one isn't found"
 touch database/database.sqlite
@@ -12,10 +12,11 @@ echo "[3/5] Installing packages using composer"
 composer install
 
 echo "[4/5] Publishing API Platform assets"
-php artisan api-platform:install
+php artisan key:generate
 
-echo "[5/5] Migrating database"
+echo "[5/5] Migrating and seed database"
 php artisan migrate --force
+php artisan db:seed
 
 echo "The app has been built and deployed!"
 
