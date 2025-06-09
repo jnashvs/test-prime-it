824866f3b9d65cb322ca19489ae6fd4e2dd67fbc
jnashvs <31808210+jnashvs@users.noreply.github.com>
2025-06-09 20:30:37 +0100

    fix update the role permission seeder

--- src/database/seeders/RolesAndPermissionsSeeder.php
+++ src/database/seeders/RolesAndPermissionsSeeder.php
@@ -8,8 +8,14 @@ use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
+        // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
         $permissions = [
@@ -24,6 +30,7 @@ class RolesAndPermissionsSeeder extends Seeder
             'view all pets',
         ];
 
+        // Create permissions if they don't exist
         foreach ($permissions as $permission) {
             Permission::firstOrCreate(
                 ['name' => $permission, 'guard_name' => 'sanctum']
@@ -57,13 +64,15 @@ class RolesAndPermissionsSeeder extends Seeder
             ],
         ];
 
-        // Create roles and assign permissions
-        foreach ($roles as $role => $rolePermissions) {
-            Role::create(['name' => $role, 'guard_name' => 'sanctum'])
-                ->givePermissionTo($rolePermissions);
-            Role::create(['name' => $role, 'guard_name' => 'web'])
-                ->givePermissionTo($rolePermissions);
+        // Create roles (if they don't exist) and assign permissions
+        foreach ($roles as $roleName => $rolePermissions) {
+            // Create or retrieve the role for 'sanctum' guard
+            $sanctumRole = Role::firstOrCreate(['name' => $roleName, 'guard_name' => 'sanctum']);
+            $sanctumRole->givePermissionTo($rolePermissions);
+
+            // Create or retrieve the role for 'web' guard
+            $webRole = Role::firstOrCreate(['name' => $roleName, 'guard_name' => 'web']);
+            $webRole->givePermissionTo($rolePermissions);
         }
     }
-
 }
